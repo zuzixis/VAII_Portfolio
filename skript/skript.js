f130718ef5abe9648e94fd070c3eee02bf8886e7
Zuzana Žillová <zuzka.zillova@gmail.com>
2021-10-14 07:35:06 +0200

    Menu,Navigácia + responzívny dizajn

---
+++ skript/skript.js
@@ -0,0 +1,27 @@
+
+function openCloseNav() {
+    var list = document.getElementById("menu-items");
+
+    if (list.style.display == "none" ){
+        list.style.display = "block";
+        document.getElementById("first-section").style.paddingTop = "300px";
+
+    }else{
+        list.style.display = "none";
+        document.getElementById("first-section").style.paddingTop = "0px";
+    }
+}
+
+/*(function() {
+    window.addEventListener('resize', function(event) {
+        const width = window.innerWidth;
+        var list = document.getElementById("menu-items");
+        if (width > 780){
+            list.style.display = "block"
+            document.getElementById("first-section").style.paddingTop = "0px";
+        }
+        if (width < 780){
+            document.getElementById("first-section").style.paddingTop = "300px";
+        }
+    });
+})();*/
