612b02759b0ecba54f4f8059dc8c8a85df9b515a
Zuzana Žillová <zuzka.zillova@gmail.com>
2021-10-15 16:19:08 +0200

    prihlásenie - oprava responzivny

--- skript/skript.js
+++ skript/skript.js
@@ -4,15 +4,15 @@ function openCloseNav() {
 
     if (list.style.display == "none" ){
         list.style.display = "block";
-        document.getElementById("first-section").style.paddingTop = "300px";
+        /*document.getElementById("first-section").style.paddingTop = "300px";*/
 
     }else{
         list.style.display = "none";
-        document.getElementById("first-section").style.paddingTop = "0px";
+        /*document.getElementById("first-section").style.paddingTop = "0px";*/
     }
 }
-
-/*(function() {
+/*
+(function() {
     window.addEventListener('resize', function(event) {
         const width = window.innerWidth;
         var list = document.getElementById("menu-items");
